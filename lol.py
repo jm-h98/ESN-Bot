09b04f1b705a065b22cb22c61aa6b426101daf95
jm-h98 <60845273+jm-h98@users.noreply.github.com>
2021-03-17 20:30:57 +0100

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,8 +1,11 @@
 import os
 import cassiopeia as cass
+from riotwatcher import LolWatcher, ApiError
 
 cass.set_riot_api_key(os.getenv('RiotAPIkey'))
 cass.set_default_region("EUW")
+watcher = LolWatcher(os.getenv('RiotAPIkey'))
+my_region = 'euw1'
 
 def get_most_played(name):
   summoner = cass.get_summoner(name=name)
@@ -10,10 +13,22 @@ def get_most_played(name):
   return [cm.champion.name for cm in good_with]
 
 
-def test(name):
+def test1(name):
   summoner = cass.get_summoner(name=name)
   current_match = cass.get_current_match(summoner)
   output = ""
   for sum in current_match:
     output = output + str(sum.name) + str(get_most_played(sum))
   return output
+
+def stats(name):
+  me = watcher.summoner.by_name(my_region, name)
+  output = me['name'] + ", Level " + str(me['summonerLevel']) + "\n"
+  my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+  for q in my_ranked_stats:
+    qtype = q['queueType']
+    if qtype == "RANKED_SOLO_5x5":
+        qtype = "SoloQ"
+    elif qtype == "RANKED_FLEX_SR":
+        qtype = "FlexQ"
+    output = output + qtype + ": " + q['tier'] + " " + q['rank'] + "\n"
