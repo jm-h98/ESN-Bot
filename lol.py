c545c2fa10b6b5c297331f94aa104acfba02e6bb
jm-h98 <60845273+jm-h98@users.noreply.github.com>
2021-03-18 22:46:15 +0100

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,109 +1,102 @@
 import os
-import cassiopeia as cass
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 
-cass.set_riot_api_key(os.getenv('RiotAPIkey'))
-cass.set_default_region("EUW")
 watcher = LolWatcher(os.getenv('RiotAPIkey'))
 my_region = 'euw1'
 
-def get_most_played(name):
-  summoner = cass.get_summoner(name=name)
-  good_with = summoner.champion_masteries.filter(lambda cm: cm.level >= 6)
-  return [cm.champion.name for cm in good_with]
+
+def get_masteries(name):
+    me = watcher.summoner.by_name(my_region, name)
+    masteries = watcher.champion_mastery.by_summoner(my_region, me['id'])
+
+    stats = []
+    for row in masteries:
+        stats_row = {'champID': row['championId'], 'points': row['championPoints']}
+        stats.append(stats_row)
+    return pd.DataFrame(stats)
 
 
-def recently_played(name):
-  me = watcher.summoner.by_name(my_region, name)
-  # check league's latest version
-  latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
-  # Lets get some champions static information
-  static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
+def get_recently_played(name, masteries, count=100):
 
-  my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
+    me = watcher.summoner.by_name(my_region, name)
 
-  # fetch last match detail
-  count = 100
-  last_matches = my_matches['matches'][0:count]
+    my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
 
-  champs = []
-  for row in last_matches:
-      champs_row = {'champID': row['champion'], 'q': row['queue']}
-      champs.append(champs_row)
+    last_matches = my_matches['matches'][0:count]
 
-  champ_dict = {}
-  for key in static_champ_list['data']:
-      row = static_champ_list['data'][key]
-      champ_dict[row['key']] = row['id']
-  for row in champs:
-      row['champID'] = champ_dict[str(row['champID'])]
-      if row['q'] == 440:
-          row['q'] = 'flex'
-      elif row['q'] == 450:
-          row['q'] = 'aram'
-      elif row['q'] == 420:
-          row['q'] = 'solo'
-      elif row['q'] == 400:
-          row['q'] = 'solo'
+    champs = []
+    for row in last_matches:
+        if row['queue'] != 450 and row['queue'] != 700:
+            champs_row = {'champID': row['champion'], 'mastery': masteries.loc[masteries['champID'] == row['champion']].iloc[0,1]}
+            champs.append(champs_row)
 
-  df = pd.DataFrame(champs)
-  df = df[(df.q != "aram")]
-  games = len(df)
-  df['frequency'] = df['champID'].map(df['champID'].value_counts())
-  df.drop_duplicates(subset=["champID"], keep="first", inplace=True)
-  df = df.sort_values('frequency', ascending=False)
-  df = df[(df.frequency > games/20)]
-  
-  output = ""
-  for i in range(0,len(df)):
-      output = output + df.iloc[i, 0] + ", " + str(int((int(df.iloc[i, 2])/games) * 100)) + "% gespielt\n"
-  return output
+    df = pd.DataFrame(champs)
+    games = len(df)
+    df['win'] = 0.0
+    df['KDA'] = 0.0
 
-def stats(name):
-  #level und name
-  me = watcher.summoner.by_name(my_region, name)
-  output = me['name'] + ", Level " + str(me['summonerLevel']) + "\n"
-  
-  #ranked stats
-  my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-  for q in my_ranked_stats:
-    qtype = q['queueType']
-    if qtype == "RANKED_SOLO_5x5":
-        qtype = "SoloQ"
-    elif qtype == "RANKED_FLEX_SR":
-        qtype = "FlexQ"
-    output = output + qtype + ": " + q['tier'] + " " + q['rank'] + "\n"
-  
-  #masteries
-  masteries = watcher.champion_mastery.by_summoner(my_region, me['id'])
-  stats = []
-  for row in masteries:
-      stats_row = {'champID': row['championId'], 'points': row['championPoints']}
-      stats.append(stats_row)
-  df = pd.DataFrame(stats)
+    for i in range(0, len(df)):
+        # try:
+        match_detail = watcher.match.by_id(my_region, last_matches[i]['gameId'])
+        champ = df.iloc[i, 0]
+        for row in match_detail['participants']:
+            if row['championId'] == champ:
+                if row['stats']['win']:
+                    df.at[i, 'win'] = "1"
+                else:
+                    df.at[i, 'win'] = "0"
+                deaths = row['stats']['deaths']
+                if deaths == 0:
+                    deaths = 1
+                kda = (row['stats']['kills'] + row['stats']['assists']) / deaths
+                df.at[i, 'KDA'] = float(kda)
 
-  # check league's latest version
-  latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
-  static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
+    df['frequency'] = df['champID'].map(df['champID'].value_counts() / games)
+    df = df.groupby('champID', as_index=False).mean()
+    col_list = list(df)
+    col_list.remove('champID')
+    col_list.remove('win')
+    df['mastery'] = df['mastery'].apply(lambda x: x * 0.01)
+    df['score'] = df[col_list].prod(axis=1)
 
-  champ_dict = {}
-  for key in static_champ_list['data']:
-      row = static_champ_list['data'][key]
-      champ_dict[row['key']] = row['id']
-  for row in stats:
-      row['championName'] = champ_dict[str(row['champID'])]
+    latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
+    static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
 
-  df = pd.DataFrame(stats)
-  df.drop('champID', axis=1, inplace=True)
-  lines = []
-  for i in range(0,10):
-      points = '{:,}'.format(int(df.iloc[i, 0])).replace(',', '.')
-      lines.append('{:<25s} {:<20s}'.format(df.iloc[i, 1], points + " Punkte"))
+    # champ static list data to dict for looking up
+    champ_dict = {}
+    for key in static_champ_list['data']:
+        row = static_champ_list['data'][key]
+        champ_dict[row['key']] = row['id']
 
-  for line in lines:
-      output = output + line + "\n"
-    
-  output = output + "\n" + recently_played(name)
+    for i in range(0, len(df)):
+        df.at[i, "champ"] = champ_dict[str(df.iloc[i, 0])]
 
-  return output
+    df = df.sort_values('score', ascending=False)
+    return df
+
+
+def stats(name, long=False):
+    me = watcher.summoner.by_name(my_region, name)
+    output = me['name'] + ", Level " + str(me['summonerLevel']) + "\n"
+
+    # ranked stats
+    my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+    for q in my_ranked_stats:
+        qtype = q['queueType']
+        if qtype == "RANKED_SOLO_5x5":
+            qtype = "SoloQ"
+        elif qtype == "RANKED_FLEX_SR":
+            qtype = "FlexQ"
+        output = output + qtype + ": " + q['tier'] + " " + q['rank'] + "\n"
+
+    scores = get_recently_played(name, get_masteries(name), 20)
+    scores = scores.head(5)
+
+    if long:
+      output = output + str(scores)
+    else:
+      for i in range(0, len(scores)):
+        output = output + '{:<20s} {:<20s}'.format(scores.iloc[i, 6], str(round(scores.iloc[i, 5], 2))) + "\n"
+
+    return output
