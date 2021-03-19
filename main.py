db4611173b242b0b9714e50e689591540e4c04bc
jm-h98 <60845273+jm-h98@users.noreply.github.com>
2021-03-19 14:16:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,30 @@ from lol import stats
 
 client = discord.Client()
 
+known_summoners = ['Nina von Toggo', 'BACK0', 'Paddy von Toggo', 'ESN UMALPH JAN', 'UWU Animegirl69', 'crrna', 'Pucinant', 'Títtíes', 'bieselmfbutzi']
+
 
 def get_help():
-  return "hola espanol?\n!csgo: Waffen/Strats für maximalen Int\n!int: Inten oder nicht?\n!champ: Random Champ\n!runes: Random Runen\n!items: Random Items\n!team: Perfekt gedraftetes Team für LoL\n!team_wild: Die etwas würzigere Alternative\n!jocho: random champ+runen+items\n!strategy: Geniale Wege, das Spiel zu drehen\n!meme: Random aktuelles Meme von Reddit, besondere subs: !cursed !oger !ich_iel !ok !history !prog !crypto\n!sub x y z ...: Random Top-Post aus einem der angegebenen Subreddits\n!best x: Zeigt die 5 aktuell besten Champs eines Summoners mit Score, der sich aus Mastery, KDA und Pickrate der letzten 20 Spiele berechnet\n!stats x: Zeigt dasselbe wie !best, nur ausführlicher"
+  return "hola espanol?\n!csgo: Waffen/Strats für maximalen Int\n!int: Inten oder nicht?\n!champ: Random Champ\n!runes: Random Runen\n!items: Random Items\n!team: Perfekt gedraftetes Team für LoL\n!team_wild: Die etwas würzigere Alternative\n!jocho: random champ+runen+items\n!strategy: Geniale Wege, das Spiel zu drehen\n!meme: Random aktuelles Meme von Reddit, besondere subs: !cursed !oger !ich_iel !ok !history !prog !crypto\n!sub x y z ...: Random Top-Post aus einem der angegebenen Subreddits\n!best x: Zeigt die 5 aktuell besten Champs eines Summoners mit Score, der sich aus Mastery, KDA und Pickrate der letzten 20 Spiele berechnet\n!lobby x: Hier die Join-Nachrichten aus dem Chat einfügen, dann werden für alle Teammates die Stats berechnet"
+
+
+def parse(data):
+  temp = []
+  for line in data.splitlines():
+    temp2 = line.split()
+    if '!lobby' in temp2:
+      temp2.remove('!lobby')
+    if 'beigetreten' in temp2:
+      temp2 = temp2[:len(temp2)-4]
+    else:
+      temp2 = temp2[:len(temp2)-3]
+    temp.append(' '.join(temp2))
+    
+  summoners = []
+  for summoner in temp:
+    if summoner not in known_summoners:
+      summoners.append(summoner)
+  return summoners
 
 
 @client.event
@@ -78,7 +99,7 @@ async def on_message(message):
       if not arg.startswith('!stats'):
         summoner = summoner + arg
     try:
-      await message.channel.send(stats(summoner, True))
+      await message.channel.send(stats(summoner))
     except Exception as e:
       await message.channel.send("Da ist wohl was schief gelaufen wie ein betrunkener Fußgänger!\n" + "[ERROR] " + str(e))
   elif message.content.startswith('!best'):
@@ -91,6 +112,17 @@ async def on_message(message):
       await message.channel.send(stats(summoner))
     except Exception as e:
       await message.channel.send("Da ist wohl was schief gelaufen wie ein betrunkener Fußgänger!\n" + "[ERROR] " + str(e))
+  elif message.content.startswith('!lobby'):
+    try:
+      summoners = parse(message.content)
+      for summoner in summoners:
+        try:
+          await message.channel.send(stats(summoner))
+        except Exception as e:
+          await message.channel.send("Da ist wohl was schief gelaufen wie ein betrunkener Fußgänger!\n" + "[ERROR] " + str(e))
+    except Exception as e:
+      await message.channel.send("Da ist wohl was schief gelaufen wie ein betrunkener Fußgänger!\n" + "[ERROR] " + str(e))
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
